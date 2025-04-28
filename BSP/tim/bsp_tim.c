05f5a9d6351f86daf33d09619c80512d774694d0
tiny_fish <i@coraa.cn>
2025-04-28 21:57:46 +0800

    feat: 增加 bsp_tim 中定时中断部分，支持配置定时中断回调。

--- BSP/tim/bsp_tim.c
+++ BSP/tim/bsp_tim.c
@@ -30,12 +30,40 @@ void bsp_tim_config(TIM_HandleTypeDef *h, double p) {
         t *= RCC_APB1_CLOCK_FREQ_MHZ;
     }
     k = ceil(t / period_limit);
-    bsp_tim_set(h, (uint16_t) (t / k) - 1, (uint16_t) k - 1);
+    bsp_tim_set(h, (uint32_t) (t / k) - 1, (uint32_t) k - 1);
 }
 
 // 0 <= blank <= 1
 // usage: bsp_tim_set_blank(&htim1, TIM_CHANNEL_1, 0.5);
 void bsp_tim_set_blank(TIM_HandleTypeDef *h, uint32_t channel, double blank) {
     BSP_ASSERT(0 <= blank && blank <= 1);
-    __HAL_TIM_SetCompare(h, channel, (uint16_t) ((h->Init.Period + 1) * blank));
+    __HAL_TIM_SetCompare(h, channel, (uint32_t) ((h->Init.Period + 1) * blank));
+}
+
+void bsp_tim_pwm_enable(TIM_HandleTypeDef *h, uint32_t channel) {
+    HAL_TIM_PWM_Start(h, channel);
+}
+
+void bsp_tim_pwm_disable(TIM_HandleTypeDef *h, uint32_t channel) {
+    HAL_TIM_PWM_Stop(h, channel);
+}
+
+// Callback Manager
+
+static uint8_t idx;
+static void (*callback[BSP_TIM_LIMIT])();
+static TIM_HandleTypeDef *handle[BSP_TIM_LIMIT];
+
+void bsp_tim_callback(TIM_HandleTypeDef *h) {
+    for(uint8_t i = 0; i < idx; i++) {
+        if(h == handle[i] && callback[i]) {
+            callback[i]();
+        }
+    }
+}
+
+void bsp_tim_it_enable(TIM_HandleTypeDef *h, void (*f)()) {
+    handle[idx] = h, callback[idx] = f;
+    HAL_TIM_Base_Start_IT(h);
+    ++ idx;
 }
